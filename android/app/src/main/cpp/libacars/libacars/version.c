b40c269fe4f20841ef5487bfcf3b0c32be49411b
SarahRose <sarahroselives@protonmail.com>
2026-06-17 14:56:14 -0400

    Vendored libacars v2.2.1 (static config), fix submodule

---
+++ android/app/src/main/cpp/libacars/libacars/version.c
@@ -0,0 +1,6 @@
+/* Generated version file for libacars static build */
+
+const char * const la_version_string = "2.2.1";
+const int la_version_major = 2;
+const int la_version_minor = 2;
+const int la_version_patch = 1;
